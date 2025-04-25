66b7789384586359d3d8c5d8881b31413c2fe253
Greg Eigen <greg.eigen@target.com>
2025-04-25 13:56:10 -0500

    modifies response header

--- src/main/kotlin/test/header/size/Controller.kt
+++ src/main/kotlin/test/header/size/Controller.kt
@@ -1,15 +1,29 @@
 package test.header.size
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 
 
 @Controller
-class Controller {
+@ExecuteOn(TaskExecutors.BLOCKING)
+class Controller (
+    val backendClient: BackendClient,
+    @Value("\${micronaut.server.netty.max-header-size}") val maxHeaderSize: Int
+) {
 
-    @Get("/headers")
-    fun index(): HttpResponse<String> {
-        return HttpResponse.ok("Response with headers")
+    @Get("/test-response-header-size")
+    fun getResponseWithHeader(): HttpResponse<String> {
+        var customResponseHeader: String = "This is a custom header value that is showing that even though the value of property micronaut.server.netty.max-header-size is set to 80, it is not being applied to this custom response header. This is evident by the fact that the response code is 200 instead of 413 despite this response header value being well over 80 bytes long."
+        return HttpResponse.ok("max-header-size set to ${maxHeaderSize}")
+            .header("custom-response-header", customResponseHeader)
+    }
+
+    @Get("/make-backend-request")
+    fun makeBackendRequest(): HttpResponse<String> {
+        return HttpResponse.ok(backendClient.getBackend())
     }
 }
